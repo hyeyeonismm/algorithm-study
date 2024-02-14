32b52e2374660bbf631ee41488f67d6ba6a727eb
hyeyeonismm <hyeyeonismm@gmail.com>
2024-02-14 16:42:05 +0900

    add: 시간 측정

--- BOJ/2012.py
+++ BOJ/2012.py
@@ -1,10 +1,13 @@
 import sys
+import time
+start_time = time.time() # 측정 시작
+
 
 n = int(input())
-rank = []
+rank = [0]*n
 cnt=0
 for i in range(n):
-    rank.append(int(sys.stdin.readline()))
+    rank[i] += (int(sys.stdin.readline()))
 
 rank.sort()
 
@@ -13,3 +16,6 @@ for i in range(1,n+1):
      cnt+=abs(i-rank[i-1])
 
 print(cnt)
+
+end_time = time.time() # 측정 종료
+print("time :", end_time - start_time) # 수행 시간 출력
