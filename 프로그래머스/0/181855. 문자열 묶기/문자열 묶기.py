9f110ddd4070ecc9b3eb5c38257326755e969724
Hyeyeon Kim <hyeyeonismm@gmail.com>
2024-04-02 15:19:12 +0900

    [level 0] Title: 문자열 묶기, Time: 9.00 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -1,10 +1,5 @@
 def solution(strArr):
-    answer = []
-    for i in range(len(strArr)):
-        strArr[i] = len(strArr[i])
-    answer=[0] * (max(strArr)+1)
-    
-    for i in range(len(strArr)):
-        answer[strArr[i]]+=1
-
+    answer=[0]*31
+    for i in strArr: 
+	    answer[len(i)]+=1
     return max(answer)
