e638043d8d1af055906a6c9ee555c3c90bf0e526
Hyeyeon Kim <hyeyeonismm@gmail.com>
2024-04-04 21:36:15 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 7.88 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -1,11 +1,13 @@
 def solution(arr, k):
     answer = []
-    for i in range(len(arr)): #3
+    for i in range(len(arr)): 
         if arr[i] not in answer: 
-            answer.append(arr[i]) #0 #1
+            answer.append(arr[i]) 
         if len(answer)>=k:
             break
+            
     if len(answer)<k:
-        for i in range(k-len(answer)):
-            answer.append(-1)
+        return answer + [-1] * (k - len(answer)) 
+    
     return answer
+
