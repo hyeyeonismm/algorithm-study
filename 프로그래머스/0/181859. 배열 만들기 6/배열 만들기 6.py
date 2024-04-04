abdc9e31a3608e96124a769303d332404b10d29a
Hyeyeon Kim <hyeyeonismm@gmail.com>
2024-04-04 20:50:13 +0900

    [level 0] Title: 배열 만들기 6, Time: 47.26 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,17 @@
+def solution(arr):
+    stk = [] #0 1 
+    i=0
+    while i < len(arr):
+        if len(stk)==0:
+            stk.append(arr[i])
+            i+=1
+        elif len(stk)!=0 and stk[-1]==arr[i]:
+            stk.pop()
+            i+=1
+        else:
+            stk.append(arr[i])
+            i+=1
+    if len(stk)==0:
+        return [-1]
+    else:
+        return stk
