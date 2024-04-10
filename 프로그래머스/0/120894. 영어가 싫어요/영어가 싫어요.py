dde709cc0c168f3254d24c792a046ebd357846ab
Hyeyeon Kim <hyeyeonismm@gmail.com>
2024-04-10 16:33:47 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,5 +1,5 @@
 def solution(numbers):
-    num = {'zero':'0','one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'}
+    num = ['zero','one','two','three','four','five','six','seven','eight','nine']
 
     for v,k in enumerate(num):
         if k in numbers:
