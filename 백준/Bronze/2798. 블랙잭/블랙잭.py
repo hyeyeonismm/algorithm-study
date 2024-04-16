7072dae382e4ab684878e20e8ebba1c08e0fe3af
Hyeyeon Kim <hyeyeonismm@gmail.com>
2024-04-16 16:55:39 +0900

    [Bronze II] Title: 블랙잭, Time: 96 ms, Memory: 37492 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -3,19 +3,12 @@ import sys
 n, m = map(int, sys.stdin.readline().split())
 card = list(map(int, sys.stdin.readline().split()))
 res = 0
+tmp = []
 
 for i in range(n):
     for j in range(i + 1, n):
-        min_v = m
         for k in range(j + 1, n):
             if card[i] + card[j] + card[k] <= m:  # 5+6+7 <= 21
-                if min_v > (m - (card[i] + card[j] + card[k])):  # 21> 3
-                    min_v = m - (card[i] + card[j] + card[k])
-                    idx = card[i] + card[j] + card[k]
-        if idx > res:
-            res = idx
-        if idx == m:
-            res = idx
-            break
+                tmp.append(card[i] + card[j] + card[k])
 
-print(res)
+print(max(tmp))
