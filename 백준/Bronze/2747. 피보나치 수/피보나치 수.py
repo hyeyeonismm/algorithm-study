544df26236308faa4a053eb4f411196043ecd2a3
Hyeyeon Kim <hyeyeonismm@gmail.com>
2024-04-23 15:47:42 +0900

    [Bronze II] Title: 피보나치 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.py
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -1,18 +1,8 @@
 import sys
 
 n = int(sys.stdin.readline())
-back = 1
-twoback = 0
+twoback, back = 0, 1
+for i in range(n):
+    twoback, back = back, back + twoback
 
-if n == 0:
-    print(0)
-elif n == 1:
-    print(1)
-
-else:
-    for i in range(2, n + 1):
-        sum_v = twoback + back  # 0+1=1 #1+1 #1+2 #2+3
-        twoback = back  # 1 #2
-        back = sum_v  # 2 #3
-
-    print(sum_v)
+print(twoback)
